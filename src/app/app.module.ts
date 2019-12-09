5e967a9e267a4d26004ffaaf7d16fadaf208f7da
Furkan <muhammet.gok@boun.edu.tr>
2019-12-09 17:51:19 +0300

    add mock data for dummy heroes and create heroes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CrisisListComponent } from "./crisis-list/crisis-list.component";
-import { HeroListComponent } from "./hero-list/hero-list.component";
+
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HeroesModule } from "./heroes/heroes.module";
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  declarations: [
-    AppComponent,
-    CrisisListComponent,
-    HeroListComponent,
-    PageNotFoundComponent
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HeroesModule],
+  declarations: [AppComponent, CrisisListComponent, PageNotFoundComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
