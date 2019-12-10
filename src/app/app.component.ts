b1fcf40a8b2d5a73789647e9e8cd98e9e2d188bb
Muhammet Furkan Gök <muhammet.gok@boun.edu.tr>
2019-12-10 17:09:41 +0300

    apply enter-leave animations for heroes list and hero detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { slideInAnimation } from "./animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
-  title = 'angular-router-sample';
+  title = "angular-router-sample";
+
+  getAnimationData(outlet: RouterOutlet) {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData["animation"]
+    );
+  }
 }
